f26fda1d968bdf6598d672c6d062c77b6d04036d
Akmal <akmalthemalong@gmail.com>
2025-09-06 11:03:41 +0800

    feat: tambahkan setup dan update command

---
+++ app/Console/Commands/SetupCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SetupCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'setup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->call('migrate');
+        $this->call('migrate:fresh', ['--seed' => true]);
+        $this->call('storage:link');
+    }
+}
